ed1288275369d4725118002eefba66f32f9d1962
Jeremy Marck <jeremymarck@MacBook-Pro-de-Jeremy.local>
2019-03-29 16:55:57 +0100

    modif

--- 2_benchmark.R
+++ 2_benchmark.R
@@ -29,16 +29,10 @@ plot(prices_desaiso) # a quoi ca correspond??
 acf(prices_desaiso) # autocorrelations??
 
 
-
-
 prices$daynum=as.integer(factor(prices$day, levels = c("lundi", "mardi", "mercredi","jeudi","vendredi","samedi","dimanche"))
 prices$daynum=daynum
                          
-
-
-
-
-#jerem
+#jerem (pas avancé)
 
 zebi=ar(prices$Zonal_Price, aic = TRUE, order.max = NULL)
 summary(zebi)
@@ -55,13 +49,110 @@ summary(zebi3)
 zebi4=ar(prices$Zonal_Price, method="yw", aic = TRUE, order.max = NULL)
 summary(zebi4)
 
+### Régression Linéaire ###
+###########################
+
+# Préliminaire pour la reg lin et les reg pénalisés :
+# Virer les variables NA
+prices2011lasso=na.omit(prices2011)
+prices2012lasso=na.omit(prices2012)
+prices2013lasso=na.omit(prices2013)
+prices201213= prices[(nrow(prices2011)+1):nrow(prices),]
+prices201213lasso=na.omit(prices201213)
+
+# Première méthode : on utilise la syntaxe des ridge/Lasso avec une pénalisation nulle
+modelelineaire=lm(Zonal_Price~daynum+prev_day_price+prev_day2_price + prev_week_price + Min_Price + Max_price + Forecasted_Zonal_Load + sqrzonalload + cubzonalload,na.action=na.omit ,data=prices2011[169:nrow(prices),])
+ytest=prices2011lasso$Zonal_Price
+ytrain=prices201213lasso$Zonal_Price
+xtrain=as.matrix(prices201213lasso[,c('daynum','prev_day_price','prev_day2_price','prev_week_price','Min_Price','Max_price','Forecasted_Zonal_Load','sqrzonalload','cubzonalload')])
+xtest=as.matrix(prices2011lasso[,c('daynum','prev_day_price','prev_day2_price','prev_week_price','Min_Price','Max_price','Forecasted_Zonal_Load','sqrzonalload','cubzonalload')])
+#modlin=lm(ytrain~xtrain,data=prices2011[169:nrow(prices2011),])
+modlin=glmnet(xtrain,ytrain,alpha=0,lambda=0)
+modlin.pred=predict(ridge.mod,s=0,newx=xtest)
+sqrt(mean((modlin.pred-ytest)^2))
+# On trouve une RMSE = 9,95
+
+# Utilisation de lm standard
+modelelineaire=lm(Zonal_Price~daynum+prev_day_price+prev_day2_price + prev_week_price + Min_Price + Max_price + Forecasted_Zonal_Load + sqrzonalload + cubzonalload,na.action=na.omit ,data=prices[169:nrow(prices),])
+summary(modelelineaire)
+coef(modelelineaire)
+fitted(modelelineaire)
+residuals(modelelineaire)
+
+# Sortie Latex avec stargazer
+install.packages("stargazer")
+install.packages("broom")
+library(stargazer)
+library(broom)
+modlin_tidy=tidy(modelelineaire)
+stargazer(modlin_tidy,type='latex',title="Résultats de la régression linéaire",summary=FALSE)
+plot(modelelineaire)
+help(stargazer)
+
+### Ridge ###
+#############
+
+# Les xtrain,test ... sont définis plus haut
+
+grid=10^seq(10,-2,length=100)
+ridge.mod=glmnet(xtrain,ytrain,alpha=0,lambda=grid)
+ridge.pred=predict(ridge.mod,s=4,newx=xtest)
+sqrt(mean((ridge.pred-ytest)^2))
+
+# Tuning du paramètre lambda
+set.seed(1)
+cv.out=cv.glmnet(xtrain,ytrain,alpha=0)
+plot(cv.out)
+bestlam=cv.out$lambda.min
+bestlam
+ridge.pred=predict(ridge.mod,s=bestlam,newx=xtest)
+sqrt(mean((ridge.pred-ytest)^2))
+out=glmnet(x=xtest,y=ytest,alpha=0)
+
+ridgesortant=predict(out,type="coefficients",s=bestlam)
+ridge_tidy=tidy(ridgesortant)
+stargazer(ridge_tidy,type='latex',title='Résultats du Ridge - Lambda optimal',summary=FALSE)
 
 
+### Estimateur LASSO ###
+########################
 
-class(prices$day)
+lasso.mod=glmnet(xtrain,ytrain,alpha=1,lambda=grid)
+plot(lasso.mod)
+set.seed(1)
+cv.out=cv.glmnet(xtrain,ytrain,alpha=1)
+plot(cv.out)
+bestlam=cv.out$lambda.min
+bestlam
+lasso.pred=predict(lasso.mod,s=bestlam,newx=xtest)
+sqrt(mean((lasso.pred-ytest)^2))
+out1=glmnet(x=xtest,y=ytest,alpha=1)
+ridgesortant=predict(out,type="coefficients",s=bestlam)
+ridge_tidy=tidy(ridgesortant)
+stargazer(ridge_tidy,type='latex',title='Résultats du Lasso - Lambda optimal',summary=FALSE)
 
+### Arbre aléatoire ###
+#######################
 
+library(MASS)
+library(tree)
+set.seed(1)
+ytest=prices2011lasso$Zonal_Price
+ytrain=prices201213lasso$Zonal_Price
+xtrain=prices201213lasso[,c('daynum','prev_day_price','prev_day2_price','prev_week_price','Min_Price','Max_price','Forecasted_Zonal_Load','sqrzonalload','cubzonalload')]
+xtest=prices2011lasso[,c('daynum','prev_day_price','prev_day2_price','prev_week_price','Min_Price','Max_price','Forecasted_Zonal_Load','sqrzonalload','cubzonalload')]
 
-
-prices$daynum=as.integer(factor(prices$day, levels = c("lundi", "mardi", "mercredi","jeudi","vendredi","samedi","dimanche")))              
-
+arbre=tree(formula=ytrain~.,data=xtrain)
+summary(arbre)
+plot(arbre)
+text(arbre,pretty=0)
+cv.arbre=cv.tree(arbre)
+plot(cv.arbre$size,cv.arbre$dev,type='b')
+prune.arbre=prune.tree(arbre,best=10)
+plot(prune.arbre)
+text(prune.arbre)
+text(prune.arbre,pretty=0)
+abline(0,1)
+yhat=predict(arbre,newdata=xtest)
+sqrt(mean((yhat-ytest)^2))
+# RMSE de 13,64
