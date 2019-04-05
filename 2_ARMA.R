191588ca5e5390e2ba81fab9c4c18e884672a003
meteve <margot_eteve@htomail.fr>
2019-04-05 18:50:43 +0200

    debut ARMA

---
+++ 2_ARMA.R
@@ -0,0 +1,21 @@
+
+library(zoo)
+
+prices <- read_csv(file = "data/tidy_prices.csv")
+
+
+#conversion en un objet serie temp
+prices.ts <- zoo(prices$Zonal_Price)
+class(prices.ts)
+
+plot(prices.ts[1:168,]) #saisonnalite journaliere, ordre 24
+
+desaison <- prices.ts - lag(prices.ts, k = -24)
+plot(desaison[1:168,])
+
+
+acf(desaison, na.action = na.pass, lag.max = 500)
+pacf(desaison, na.action = na.pass, lag.max = 500)
+
+
+
