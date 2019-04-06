c31f14d781c51df93140a4147e64537085147cc2
Jeremy Marck <jeremymarck@MacBook-Pro-de-Jeremy.local>
2019-04-06 14:25:34 +0200

    Nettoyage du code

--- 0_data_import_0310.R
+++ 0_data_import_0310.R
@@ -9,7 +9,6 @@ library(fastDummies)
 
 # IMPORTATION DES DONNEES -------------------------------------------------
 
-
 # importer en convertissant directement en format 'datetime' la deuxieme colonne
 prices <- read_delim("data/prices.csv", delim = ";",
                      col_types = cols('timestamp' = 
@@ -23,12 +22,6 @@ prices <- rename(prices, ZONEID = ZONEID, timestamp = timestamp,
                  Forecasted_Zonal_Load = `Forecasted Zonal Load`,
                  Zonal_Price = `Zonal Price`)
 
-
-
-
-
-
-
 # CREATION DE VARIABLES ---------------------------------------------------
 
 ############## DUMMIES
@@ -37,11 +30,9 @@ prices <- rename(prices, ZONEID = ZONEID, timestamp = timestamp,
 prices$year <- year(prices$timestamp)
 prices <- dummy_cols(prices, select_columns = 'year')
 
-
 # dummies pour le mois
 prices$month <- month(prices$timestamp)
 prices <- dummy_cols(prices, select_columns = 'month')
-
 
 # dummies pour le jour
 prices <- prices %>%
@@ -51,24 +42,16 @@ prices$day <- tolower(prices$day)
 
 prices <- dummy_cols(prices, select_columns = 'day')
 
-
 # dummies pour le week-end 
 prices$samedi<-as.numeric(prices$day_samedi==1)
 prices$dimanche<-as.numeric(prices$day_dimanche==1)
 prices$weekend<-(prices$samedi + prices$dimanche)
 
-
 # dummies pour l'heure
 prices$hour <- hour(prices$timestamp)
 prices <- dummy_cols(prices, select_columns = 'hour')
 
-
-
-
-
-
-
-############## AUTRES
+####### AUTRES
 
 # prix a la journee precedente 
 previous_day_price <- apply(as.matrix(25:nrow(prices)), 1,
@@ -77,14 +60,12 @@ previous_day_price_NA <- rep(NA, 24)
 
 prices$prev_day_price <- c(previous_day_price_NA, previous_day_price)
 
-
 # prix à deux jours avant 
 previous_day2_price <- apply(as.matrix(49:nrow(prices)), 1,
                              function(i){prices$Zonal_Price[(i-48)]})
 previous_day2_price_NA <- rep(NA, 48)
 
 prices$prev_day2_price <- c(previous_day2_price_NA, previous_day2_price)
-
 
 # prix a la semaine precedente
 previous_week_price <- apply(as.matrix(169:nrow(prices)), 1,
@@ -93,7 +74,6 @@ previous_week_price_NA <- rep(NA, 168)
 
 prices$prev_week_price <- c(previous_week_price_NA, previous_week_price)
 
-
 # prix minimum des 24h précédentes
 Min_Price <- apply(as.matrix(25:nrow(prices)), 1,
                    function(i){min(prices$Zonal_Price[(i-24):i])})
@@ -101,14 +81,12 @@ Min_Price_NA <- rep(NA, 24)
 
 prices$Min_Price <- c(Min_Price_NA, Min_Price)
 
-
 # prix maximum des 24h précédentes
 Max_price <- apply(as.matrix(25:nrow(prices)), 1,
                    function(i){max(prices$Zonal_Price[(i-24):i])})
 Max_price_NA<-rep(NA,24)
 
 prices$Max_price<-c(Max_price_NA, Max_price)
-
 
 # load au carré (Total Load)
 prices$sqrtotalload <- (prices$Forecasted_Total_Load)^2
@@ -126,10 +104,6 @@ prices$cubzonalload <- (prices$Forecasted_Zonal_Load)^3
 prices$daynum <- as.integer(factor(prices$day,
                                    levels = c("lundi", "mardi", "mercredi",
                                               "jeudi","vendredi","samedi","dimanche")))
-
-
-
-
 
 # EXPORTER DONNEES --------------------------------------------------------
 
