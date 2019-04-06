8a4f66a463fc499471c2d9e1f001374446ecdef3
meteve <margot_eteve@htomail.fr>
2019-04-06 16:07:10 +0200

    table des donnees a predire

--- 0_data_import_0310.R
+++ 0_data_import_0310.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(mgcv)
 library(lubridate)
 library(fastDummies)
+library(plyr)
 
 #rm(list=ls())
 
@@ -17,7 +18,7 @@ prices <- read_delim("data/prices.csv", delim = ";",
 
 
 # on remplace les espaces par des underscore dans les noms de colonnes
-prices <- rename(prices, ZONEID = ZONEID, timestamp = timestamp,
+prices <- dplyr::rename(prices, ZONEID = ZONEID, timestamp = timestamp,
                  Forecasted_Total_Load = `Forecasted Total Load`,
                  Forecasted_Zonal_Load = `Forecasted Zonal Load`,
                  Zonal_Price = `Zonal Price`)
@@ -111,6 +112,30 @@ prices2012 <- filter(prices, grepl(pattern = '2012.', timestamp))
 prices2013 <- filter(prices, grepl(pattern = '2013.', timestamp))
 
 
+# TABLES A PREDIRE --------------------------------------------------------
+
+#fonction qui retourne pour une date donnee la table des variables a chaque heure
+get_df_pred <- function(x){
+  df_pred <- filter(prices, grepl(pattern = x, timestamp))
+  return(df_pred)
+}
+
+#date a predire
+date_pred <- c('2013-06-06', '2013-06-17', '2013-06-24', '2013-07-04',
+               '2013-07-09', '2013-07-13', '2013-07-16', '2013-07-18',
+               '2013-07-19', '2013-07-20', '2013-07-24', '2013-07-25',
+               '2013-12-06', '2013-12-07', '2013-12-17')
+
+#liste des tables de donnees pour chaque date a predire
+list_df_pred <- lapply(date_pred, get_df_pred)
+#dataframe des dates a predire
+df_pred <- ldply(list_df_pred)
+
+
 # EXPORTER DONNEES --------------------------------------------------------
 
+write_csv(df_pred, "data/df_pred.csv")
 write_csv(prices, "data/tidy_prices.csv")
+
+
+
