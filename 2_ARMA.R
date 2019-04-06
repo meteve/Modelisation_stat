04e9d6eba1dbe8759d139eed0cc9439cf724f610
meteve <margot_eteve@htomail.fr>
2019-04-06 14:54:11 +0200

    arma

--- 2_ARMA.R
+++ 2_ARMA.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 library(zoo)
 
 prices <- read_csv(file = "data/tidy_prices.csv")
@@ -7,15 +7,15 @@ prices <- read_csv(file = "data/tidy_prices.csv")
 #conversion en un objet serie temp
 prices.ts <- zoo(prices$Zonal_Price)
 class(prices.ts)
-
+plot(prices.ts)
 plot(prices.ts[1:168,]) #saisonnalite journaliere, ordre 24
 
 desaison <- prices.ts - lag(prices.ts, k = -24)
 plot(desaison[1:168,])
 
 
-acf(desaison, na.action = na.pass, lag.max = 500)
-pacf(desaison, na.action = na.pass, lag.max = 500)
+acf(desaison, na.action = na.pass, lag.max = 50000)
+pacf(desaison, na.action = na.pass, lag.max = 1000)
 
 
 
