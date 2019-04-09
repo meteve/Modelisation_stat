4823dcacf3ba0b9d35554cc3ba76991e409d35d3
meteve <margot_eteve@htomail.fr>
2019-04-09 17:09:05 +0200

    fonction gam a revoir

--- 3_gam.R
+++ 3_gam.R
@@ -1,7 +1,7 @@
 
 library(tidyverse)
 library(mgcv)
-
+library(stargazer)
 
 prices <- read_csv(file = "data/tidy_prices.csv")
 df_pred <- read_csv(file = "data/df_pred.csv")
@@ -16,54 +16,55 @@ df_pred <- read_csv(file = "data/df_pred.csv")
 # plot(gam) donne des plots pour les effets NON LINEAIRES
 
 
-gam_1 <- gam(formula = Zonal_Price ~ s(daynum,k=7,bs="cc") + s(prev_day_price,k=50,bs="tp") +
-               s(prev_week_price,k=50,bs="tp") + s(Min_Price,k=50,bs="tp") +
-               s(Max_price,k=50,bs="tp") + s(sqrzonalload,k=50,bs="tp") +
-               s(Forecasted_Zonal_Load) + s(cubzonalload,k=100,bs="tp"), 
-             data = prices[169:nrow(prices),])
-
-names(gam_1)
-summary(gam_1)
-plot(gam_1)
+# gam_1 <- gam(formula = Zonal_Price ~ s(daynum,k=7,bs="cc") + s(prev_day_price,k=50,bs="tp") +
+#                s(prev_week_price,k=50,bs="tp") + s(Min_Price,k=50,bs="tp") +
+#                s(Max_price,k=50,bs="tp") + s(sqrzonalload,k=50,bs="tp") +
+#                s(Forecasted_Zonal_Load) + s(cubzonalload,k=100,bs="tp"), 
+#              data = prices[169:nrow(prices),])
+# 
+# names(gam_1)
+# summary(gam_1)
+# plot(gam_1)
 
 
 
 
 
 #on ajoute des variables
-gam_2 <- gam(formula = Zonal_Price ~ s(daynum,k=7,bs="cc") + s(month,k=12,bs='cc') +
-               s(prev_day_price,k=50,bs="tp") + s(hour, k=24, bs='cc') +
-               s(prev_week_price,k=50,bs="tp") + s(Min_Price,k=50,bs="tp")+
-               s(Max_price,k=50,bs="tp") + s(sqrzonalload,k=50,bs="tp") + 
-               s(Forecasted_Zonal_Load,bs="tp") + s(cubzonalload,k=100,bs="tp") +
-               s(Forecasted_Total_Load) + s(sqrtotalload,k=50,bs="tp") +
-               s(cubtotalload,k=100,bs="tp"), 
-             data = prices[169:nrow(prices),])
+# gam_ok <- gam(formula = Zonal_Price ~ s(daynum,k=7,bs="cc") + s(month,k=12,bs='cc') +
+#                s(prev_day_price,k=50,bs="tp") + s(hour, k=24, bs='cc') +
+#                s(prev_week_price,k=50,bs="tp") + s(Min_Price,k=50,bs="tp")+
+#                s(Max_price,k=50,bs="tp") + s(sqrzonalload,k=50,bs="tp") +
+#                s(Forecasted_Zonal_Load,bs="tp") + s(cubzonalload,k=100,bs="tp") +
+#                s(Forecasted_Total_Load) + s(sqrtotalload,k=50,bs="tp") +
+#                s(cubtotalload,k=100,bs="tp"),
+#              data = prices[169:nrow(prices),])
 
-
-summary(gam_2)
-gam.check(gam_2)
+# 
+# summary(gam_2)
+# gam.check(gam_2)
 
 # en changeant les degres de liberte
-gam_3 <- gam(formula = Zonal_Price ~ s(daynum,k=7,bs="cc") + s(month,k=12,bs='cc') +
-               s(prev_day_price,k=50,bs="tp") + s(hour, k=24, bs='cc') +
-               s(prev_week_price,k=50,bs="tp") + s(Min_Price,k=50,bs="tp")+
-               s(Max_price,k=50,bs="tp") + s(sqrzonalload,k=50,bs="tp") + 
-               s(Forecasted_Zonal_Load, bs="tp") + s(cubzonalload,k=100,bs="tp") +
-               s(Forecasted_Total_Load, bs="tp") + s(sqrtotalload,k=50,bs="tp") +
-               s(cubtotalload,k=100,bs="tp"), 
-             data = prices[169:nrow(prices),])
-
-summary(gam_3)
-
-gam.check(gam_3)
+# gam_3 <- gam(formula = Zonal_Price ~ s(daynum,k=7,bs="cc") + s(month,k=12,bs='cc') +
+#                s(prev_day_price,k=50,bs="tp") + s(hour, k=24, bs='cc') +
+#                s(prev_week_price,k=50,bs="tp") + s(Min_Price,k=50,bs="tp")+
+#                s(Max_price,k=50,bs="tp") + s(sqrzonalload,k=50,bs="tp") + 
+#                s(Forecasted_Zonal_Load, bs="tp") + s(cubzonalload,k=100,bs="tp") +
+#                s(Forecasted_Total_Load, bs="tp") + s(sqrtotalload,k=50,bs="tp") +
+#                s(cubtotalload,k=100,bs="tp"), 
+#              data = prices[169:nrow(prices),])
+# 
+# summary(gam_3)
+# 
+# gam.check(gam_3)
 
 
 #creer le vecteur des prix a predire
+# total_pred <- predict(gam_2, newdata = df_pred)
 
-total_pred <- predict(gam_2, newdata = df_pred)
 
 
+# fonctions qui retournent la rmse et la mae pour y et yhat donnes
 get_rmse <- function(y, yhat){
   rmse <- sqrt(mean((y-yhat)^2))
   return(rmse)
@@ -74,7 +75,30 @@ get_mae <- function(y,yhat){
   return(mae)
 }
 
-get_rmse(total_pred, data$Zonal_Price)
 
-class(total_pred)
+
+# fonction qui estime le modele gam pour une date donnee sur toutes les dates ulterieures
+
+get_gam <- function(date){
+  index <- which(grepl(pattern = date, prices$timestamp))[1]
+  gam <- gam(formula = Zonal_Price ~ s(daynum,k=7,bs="cc") + s(month,k=12,bs='cc') +
+                       s(prev_day_price,k=50,bs="tp") + s(hour, k=24, bs='cc') +
+                       s(prev_week_price,k=50,bs="tp") + s(Min_Price,k=50,bs="tp")+
+                       s(Max_price,k=50,bs="tp") + s(sqrzonalload,k=50,bs="tp") +
+                       s(Forecasted_Zonal_Load, bs="tp") + s(cubzonalload,k=100,bs="tp") +
+                       s(Forecasted_Total_Load, bs="tp") + s(sqrtotalload,k=50,bs="tp") +
+                       s(cubtotalload,k=100,bs="tp"),
+                     data = prices[169:(index-1),])
+  pred <- predict(gam, newdata = filter(df_pred, grepl(pattern = date, prices$timestamp)))
+  y <- filter(prices, grepl(pattern = date, prices$timestamp))$Zonal_Price
+  rmse <- get_rmse(y = y, yhat = pred)
+  mae <- get_mae(y = y, yhat = pred)
+  #return('gam' = gam, 'pred' = pred, 'rmse' = rmse, 'mae' = mae)
+}
+
+
+get_gam(date = '2013-06-06')
+
+
+
 
