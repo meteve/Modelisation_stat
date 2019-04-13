509277e09299fe6a85102e3359c0c5d1361e36a1
meteve <margot_eteve@htomail.fr>
2019-04-13 16:37:42 +0200

    debut code a rendre

---
+++ A_RENDRE.R
@@ -0,0 +1,10 @@
+# Les résultats des modèles ont été exportés lorsque nous avons fait tourner notre code.
+# Ces résultats sont donc ici seulement importés et les boucles qui estiment les modèles
+# ont été mises en commentaires pour que le code s'exécute rapidement'.
+
+
+# Les packages nécessaires
+
+
+
+
